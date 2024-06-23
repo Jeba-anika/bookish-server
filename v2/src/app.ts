bbe7027652d19803e6ae8d2fcc0c51d78efdb63f
Jeba Anika <anikanur98@gmail.com>
2024-06-24 00:24:26 +0600

    chore: working on initial setups

--- v2/src/app.ts
+++ v2/src/app.ts
@@ -1,9 +1,13 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import router from "./app/routes";
+
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v2", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
---
+++ v2/src/app/modules/buyers/buyers.interface.ts
@@ -0,0 +1,9 @@
+export type TBuyer = {
+  id: string;
+  name: string;
+  email: string;
+  contactNo: string;
+  address: string;
+  profileImage?: string;
+  isDeleted: boolean;
+};
